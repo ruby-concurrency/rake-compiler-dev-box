1db568740ada62b9ddc3b3a0aaad32ed8aaab45b
Jerry D'Antonio <stumpjumper@gmail.com>
2014-06-14 22:01:16 -0400

    Added tests for Windows.

--- tests/windows-test.cmd
+++ tests/windows-test.cmd
@@ -63,12 +63,12 @@ CALL jgem install %PKG%\concurrent-ruby-%VERSION%-java.gem --no-ri --no-rdoc
 CALL jruby %EXAMPLES%/bench_atomic.rb
 CALL jgem uninstall concurrent-ruby -I -a -x
 
-IF X64==0 GOTO:eof
+IF %X64==0 GOTO Eof
 REM #################################################################
 REM Ruby 2.0 (64 bit)
 ECHO '###############################################################'
 
-SET PATH=C:\Ruby200x64\bin;%ORIGINAL_PATH%
+SET PATH=C:\Ruby200-x64\bin;%ORIGINAL_PATH%
 
 CALL ruby -v
 CALL ruby C:\Ruby200x64\DevKit\dk.rb init
@@ -85,3 +85,17 @@ CALL gem uninstall concurrent-ruby -I -a -x
 CALL gem install %PKG%\concurrent-ruby-%VERSION%.gem --no-ri --no-rdoc
 CALL ruby %EXAMPLES%/bench_atomic.rb
 CALL gem uninstall concurrent-ruby -I -a -x
+
+REM #################################################################
+REM JRuby 1.7.2 (64 bit)
+ECHO '###############################################################'
+
+SET PATH=c:\jruby-1.7.12x64\bin;%ORIGINAL_PATH%
+
+CALL jruby -v
+
+CALL jgem install %PKG%\concurrent-ruby-%VERSION%-java.gem --no-ri --no-rdoc
+CALL jruby %EXAMPLES%/bench_atomic.rb
+CALL jgem uninstall concurrent-ruby -I -a -x
+
+:Eof
