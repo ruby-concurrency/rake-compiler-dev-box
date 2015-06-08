fce5b6a4d5d61a617bf53de8a159402893a32091
Jerry D'Antonio <stumpjumper@gmail.com>
2015-06-08 08:31:21 -0400

    Released v0.9.0.pre2

--- publish-concurrent-ruby.rb
+++ publish-concurrent-ruby.rb
@@ -9,18 +9,12 @@ GEMS = [
   "concurrent-ruby-ext-#{Concurrent::VERSION}.gem",
   "concurrent-ruby-ext-#{Concurrent::VERSION}-x86-mingw32.gem",
   "concurrent-ruby-ext-#{Concurrent::VERSION}-x64-mingw32.gem",
-  #"concurrent-ruby-ext-#{Concurrent::VERSION}-x86-linux.gem",
-  #"concurrent-ruby-ext-#{Concurrent::VERSION}-x86_64-linux.gem",
-  #"concurrent-ruby-ext-#{Concurrent::VERSION}-x86_64-darwin-14.gem",
-  #"concurrent-ruby-ext-#{Concurrent::VERSION}-x86-solaris-2.11.gem",
+  "concurrent-ruby-edge-#{Concurrent::EDGE_VERSION}.gem",
 ]
 
-Dir["concurrent-ruby/pkg/concurrent-ruby-*#{Concurrent::VERSION}*.gem"].each do |gem|
-  basename = File.basename(gem)
-  if GEMS.include?(basename)
-    puts "Publishing #{basename}..."
-    `gem push #{gem}`
-  else
-    warn "Skipping unsupported package #{basename}..."
-  end
+GEMS.each do |gem|
+  file = File.join("concurrent-ruby/pkg", gem)
+  basename = File.basename(file)
+  puts "Publishing #{basename}..."
+  `gem push #{file}`
 end
